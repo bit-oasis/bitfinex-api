48ff2403dcfde53ec2b208de1a268d7c417b8ea5
Daniel Robenek <daniel.robenek@me.com>
2017-10-19 13:24:37 +0200

    Set logger to auth channel as well

--- src/BitOasis/Bitfinex/Websocket/BitfinexWebsocket.php
+++ src/BitOasis/Bitfinex/Websocket/BitfinexWebsocket.php
@@ -9,6 +9,7 @@ use BitOasis\Bitfinex\Websocket\Channel\Authenticated\AuthenticatedSubchannel;
 use Nette\Utils\Json;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Ratchet\Client\Connector;
 use Ratchet\Client\WebSocket;
@@ -52,6 +53,11 @@ class BitfinexWebsocket implements LoggerAwareInterface {
 		$this->subscribers[] = $this->authChannel;
 		$this->loop = $loop;
 		$this->logger = new NullLogger();
+	}
+
+	public function setLogger(LoggerInterface $logger) {
+		$this->authChannel->setLogger($logger);
+		$this->logger = $logger;
 	}
 
 	/**
