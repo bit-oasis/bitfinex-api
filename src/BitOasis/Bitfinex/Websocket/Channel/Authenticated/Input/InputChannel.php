7f9049a048a4801bdd51f960ced458af8e85c8de
Daniel Robenek <daniel.robenek@me.com>
2018-01-30 10:27:15 +0100

    Fixed missing default logger

--- src/BitOasis/Bitfinex/Websocket/Channel/Authenticated/Input/InputChannel.php
+++ src/BitOasis/Bitfinex/Websocket/Channel/Authenticated/Input/InputChannel.php
@@ -10,6 +10,7 @@ use BitOasis\Bitfinex\Websocket\Channel\Authenticated\ConnectionAuthenticatedSub
 use Nette\Utils\Json;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
 use React\Promise\Promise;
 
 /**
@@ -22,6 +23,10 @@ class InputChannel extends ConnectionAuthenticatedSubchannelAdapter implements L
 	protected $operationsByReplyCode = [];
 
 	protected $multiOperations = [];
+
+	public function __construct() {
+		$this->logger = new NullLogger();
+	}
 
 	public function onWebsocketMessageReceived($data) {
 		if ($data[0] === 0 && $data[1] === 'n') {
